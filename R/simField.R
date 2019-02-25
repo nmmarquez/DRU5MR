bab5ce8b4241ce200c770fd56b0043e83162ece9
Neal Marquez <nmmarquez@gmail.com>
2019-02-25 08:31:35 -0800

    update sim field actions

--- R/simField.R
+++ R/simField.R
@@ -1,123 +1,127 @@
-rm(list=ls())
+#' Simulate A continous 2D spatial field evolving over time 
+#'
+#' @description Calculates a continous underlying spatial field for the
+#' Dominican Republic over time and across ages for probability of mortality
+#' for one of 7 age groups. The final observed field is a linear additive model
+#' as described in Wakefield 2017.
+#' 
+#' @param M Number of years
+#' @param sigmaE spatial variance
+#' @param rangeE spatial range
+#' @param rhoAR Yearly autoregression
+#' @param sigmaRW Variation of independent temporal component
+#' @param ageB age specfifc effects
+#' @param deltaUrban urban effect
+#' @param offset mesh creation parameter
+#' @param max.edge mesh creation parameter
+#'
+#' @return field object, contains multiple items. Spatial points data frame with 
+#' raster values for transformed linear combination, and other values. A mesh 
+#' that was used to create the latent field. The latent field
+#' itself. A bounding shape where all observations take place. A projection 
+#' matrix from the mesh to the entire field of interest. The spde for the matern
+#' approximation.
+#'
+#' @examples
+#' \dontrun{
+#' require(tidyverse)
+#'
+#' fieldDR <- simField(4, rangeE = 1.3)
+#'  
+#' fieldDR$fqzDF %>%
+#'     ggplot(aes(x=long, y=lat, fill=fqz)) +
+#'     geom_raster() +
+#'     facet_wrap(~year) +
+#'     coord_equal() +
+#'     theme_void() +
+#'     scale_fill_distiller(palette = "Spectral")
+#'
+#' @export
 
-library(haven)
-library(rgdal)
-library(sp)
-library(maptools)
-library(PointPolygon)
-library(rgeos)
-library(raster)
-library(tidyverse)
-library(INLA)
-library(ar.matrix)
+
+rm(list=ls())
 
 load("./Data/prepData.Rdata")
 
-plot(meshDR <- inla.mesh.2d(
-    loc=bbCoords(spDF), 
-    offset = c(1),
-    max.edge = c(.3,.6)))
-
-AprojDR <- INLA::inla.spde.make.A(
-    mesh=meshDR, 
-    loc=as.matrix(fullDF[,c("long", "lat")]))
-
-M <- 9
-rangeE <- .4
-sigmaE <- .04
-
-kappaE <- sqrt(8) / rangeE
-tauE <- 1/(sqrt(4*pi)*kappaE*sigmaE)
-spde <- INLA::inla.spde2.matern(meshDR)
-Qspde <- tauE**2 * (kappaE**4 * spde$param.inla$M0 +
-                    2 * kappaE**2 *spde$param.inla$M1 + spde$param.inla$M2)
-Qar1 <- Q.AR1(M, 1, .9) 
-Q <- kronecker(Qar1, Qspde)
-x_ <- as.vector(ar.matrix::sim.AR(n=1, Q))
-x <- x_ - mean(x_)
-
-for(i in 1:M){
-    fullDF[,paste0("x", i)] <- as.vector(
-        AprojDR %*% x[(meshDR$n * (i-1) + 1):(meshDR$n * i)])
-}
-
-pAgeDF <- tibble(
-    ageB = arm::logit(c(.007, .006, .005, .004, .003, .002, .001) + .00025),
-    age  = 1:7)
-
-pTimeDF <- tibble(
-    time = 1:M,
-    rwTime = c(r.AR1(1, M, .001, .9999) %>% `-`(mean(.)))
-)
-
-fullDF <- fullDF %>%
-    gather(key="time", value="x", -long, -lat, -reg, -urban, -id, -strat) %>%
-    mutate(time=as.integer(gsub("x", "", time))) %>%
-    right_join(expand.grid(id=1:max(fullDF$id), time=1:M, age=1:7)) %>%
-    left_join(pTimeDF) %>%
-    left_join(pAgeDF) %>%
-    mutate(deltaU=ifelse(urban == 1, -.01, 0)) %>%
-    mutate(p = arm::invlogit(ageB + deltaU + rwTime + x))
-
-fqzDF <- fullDF %>%
-    group_by(long, lat, id, reg, urban, strat, time) %>%
-    summarize(fqz=1-prod(1-p)) %>%
-    ungroup
-
-fqzDF %>%
-    mutate(long=round(long, 5), lat=round(lat, 5)) %>%
-    ggplot(aes(x=long, y=lat, fill=fqz)) +
-    geom_raster() +
-    facet_wrap(~time) +
-    coord_equal() +
-    theme_void() +
-    scale_fill_distiller(palette = "Spectral")
-
-summary(fqzDF)
-
-rasterFull <- stack(lapply(1:M, function(i){
-    spFullDF <- fqzDF %>%
-        filter(time == i) %>%
-        select(long, lat, fqz)
-    coordinates(spFullDF) <- ~ long + lat
-    proj4string(spFullDF) <- proj4string(spDF)
-    gridded(spFullDF) <- TRUE
-    rasterDF <- raster(spFullDF)
-    rasterDF
-}))
-
-plot(rasterFull, legend=FALSE)
-
-simField <- function(N=60, sigmaE=1, rangeE=.3, shape=NULL,
-                     beta0=0, betaList=list(), link=arm::invlogit, ...){
-    if(is.null(shape)){
-        shape <- sp::SpatialPolygons(list(sp::Polygons(list(sp::Polygon(
-            matrix(c(0,1,1,0,0,0,1,1), ncol=2))), 1)))
-    }
-    bb <- shape@bbox
-    shape$isPresent <- TRUE
-    baseRaster <- raster::raster(
-        ncols=N, nrows=round(N*hRatio(shape)),
-        xmn=bb[1,1], xmx=bb[1,2], ymn=bb[2,1], ymx=bb[2,2],
-        crs=shape@proj4string)
-    shapeRaster <- raster::rasterize(shape, baseRaster, field=1)
-    shapeExtPointsDF <- sp::SpatialPointsDataFrame(
-        sp::coordinates(shapeRaster),
-        data=as.data.frame(sp::coordinates(shapeRaster)),
-        proj4string = shape@proj4string)
+simField <- function(
+    M,
+    rangeE = .8,
+    sigmaE = .04,
+    rhoAR = .9,
+    sigmaRW = .001,
+    ageB = arm::logit((7:1 / 1000) + .00025),
+    deltaUrban = -.1,
+    offset = 1.,
+    max.edge = c(.15,.3)){
     
-    validIndex <- !is.na(sp::over(shapeExtPointsDF, shape)$isPresent)
-    shapePointsDF <- shapeExtPointsDF[validIndex,]
-    mesh <- INLA::inla.mesh.2d(loc=bbCoords(shape), ...)
-    AprojField <- INLA::inla.spde.make.A(mesh=mesh, loc=shapePointsDF)
+    library(rgdal)
+    library(sp)
+    library(maptools)
+    library(PointPolygon)
+    library(rgeos)
+    library(tidyverse)
+    library(ar.matrix)
+    
+    meshDR <- INLA::inla.mesh.2d(
+        loc=bbCoords(spDF), 
+        offset = offset,
+        max.edge = max.edge)
+    
+    AprojDR <- INLA::inla.spde.make.A(
+        mesh=meshDR, 
+        loc=as.matrix(fullDF[,c("long", "lat")]))
     
     kappaE <- sqrt(8) / rangeE
     tauE <- 1/(sqrt(4*pi)*kappaE*sigmaE)
-    spde <- INLA::inla.spde2.matern(mesh)
-    Q <- tauE**2 * (kappaE**4 * spde$param.inla$M0 +
-                        2 * kappaE**2 *spde$param.inla$M1 + spde$param.inla$M2)
+    spde <- INLA::inla.spde2.matern(meshDR)
+    Qspde <- tauE**2 * 
+        (kappaE**4 * spde$param.inla$M0 +
+             2 * kappaE**2 *spde$param.inla$M1 + spde$param.inla$M2)
+    Qar1 <- Q.AR1(M, 1, rhoAR) 
+    Q <- kronecker(Qar1, Qspde)
     x_ <- as.vector(ar.matrix::sim.AR(n=1, Q))
     x <- x_ - mean(x_)
-    N <- nrow(shapePointsDF@data)
+    
+    for(i in 1:M){
+        fullDF[,paste0("x", i)] <- as.vector(
+            AprojDR %*% x[(meshDR$n * (i-1) + 1):(meshDR$n * i)])
+    }
+    
+    pAgeDF <- tibble(
+        ageB = ageB,
+        age  = 1:7)
+    
+    pTimeDF <- tibble(
+        time = 1:M,
+        rwTime = c(r.AR1(1, M, sigmaRW, .9999) %>% `-`(mean(.)))
+    )
+    
+    idDF <- as_tibble(expand.grid(id=1:max(fullDF$id), time=1:M, age=1:7))
+    
+    fullDF <- fullDF %>%
+        gather("time", "x", -long, -lat, -reg, -urban, -id, -strat) %>%
+        mutate(time=as.integer(gsub("x", "", time))) %>%
+        right_join(idDF, by=c("id", "time")) %>%
+        left_join(pTimeDF, by="time") %>%
+        left_join(pAgeDF, by="age") %>%
+        mutate(deltaU=ifelse(urban == 1, deltaUrban, 0)) %>%
+        mutate(p = arm::invlogit(ageB + deltaU + rwTime + x)) %>%
+        mutate(year = time + 1999) %>%
+        select(-time)
+    
+    fqzDF <- fullDF %>%
+        group_by(long, lat, id, reg, urban, strat, year) %>%
+        summarize(fqz=1-prod(1-p)) %>%
+        ungroup
+    
+    boundShape <- rgeos::gUnaryUnion(spDF, id =  spDF@data$Bound)
+    boundShape <- sp::SpatialPolygonsDataFrame(
+        boundShape, data.frame(Bound = 1, row.names = "1"))
+    
+    field <- list(spdf = fullDF, mesh = meshDR, latent = x, fqzDF=fqzDF,
+                  bound = boundShape, AprojField = AprojDR, spde = spde, 
+                  deltaUrban, rangeE = rangeE, sigmaE = sigmaE, rhoAR = rhoAR,
+                  sigmaRW = sigmaRW, ageB = ageB)
+    class(field) <- "field"
+    field
 }
-
